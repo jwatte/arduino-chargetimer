3522d48532c765e26892c5817035fd778b75af43
Jon Watte <jwatte@gmail.com>
2011-12-30 13:26:50 -0800

    A simple device command tester

---
+++ GenerateSerial/AVRemoteExplorerForm.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace GenerateSerial
+{
+    partial class AVRemoteExplorerForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.serialPortAVR = new System.IO.Ports.SerialPort(this.components);
+            this.listBoxInfo = new System.Windows.Forms.ListBox();
+            this.labelToRead = new System.Windows.Forms.Label();
+            this.labelToWrite = new System.Windows.Forms.Label();
+            this.timerPoll = new System.Windows.Forms.Timer(this.components);
+            this.buttonReset = new System.Windows.Forms.Button();
+            this.buttonStatus = new System.Windows.Forms.Button();
+            this.textBoxCarrier = new System.Windows.Forms.TextBox();
+            this.textBoxDutyCycle = new System.Windows.Forms.TextBox();
+            this.buttonCarrier = new System.Windows.Forms.Button();
+            this.textBoxTargetNEC = new System.Windows.Forms.TextBox();
+            this.textBoxCmdNEC = new System.Windows.Forms.TextBox();
+            this.buttonSendNEC = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // serialPortAVR
+            // 
+            this.serialPortAVR.BaudRate = 57600;
+            this.serialPortAVR.DtrEnable = true;
+            this.serialPortAVR.Handshake = System.IO.Ports.Handshake.RequestToSend;
+            this.serialPortAVR.PortName = "COM3";
+            this.serialPortAVR.ReadTimeout = 100;
+            this.serialPortAVR.RtsEnable = true;
+            this.serialPortAVR.WriteTimeout = 100;
+            // 
+            // listBoxInfo
+            // 
+            this.listBoxInfo.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBoxInfo.ItemHeight = 16;
+            this.listBoxInfo.Location = new System.Drawing.Point(119, 13);
+            this.listBoxInfo.Name = "listBoxInfo";
+            this.listBoxInfo.Size = new System.Drawing.Size(748, 532);
+            this.listBoxInfo.TabIndex = 1;
+            // 
+            // labelToRead
+            // 
+            this.labelToRead.AutoSize = true;
+            this.labelToRead.Location = new System.Drawing.Point(12, 13);
+            this.labelToRead.Name = "labelToRead";
+            this.labelToRead.Size = new System.Drawing.Size(46, 17);
+            this.labelToRead.TabIndex = 2;
+            this.labelToRead.Text = "label1";
+            // 
+            // labelToWrite
+            // 
+            this.labelToWrite.AutoSize = true;
+            this.labelToWrite.Location = new System.Drawing.Point(12, 39);
+            this.labelToWrite.Name = "labelToWrite";
+            this.labelToWrite.Size = new System.Drawing.Size(46, 17);
+            this.labelToWrite.TabIndex = 3;
+            this.labelToWrite.Text = "label2";
+            // 
+            // timerPoll
+            // 
+            this.timerPoll.Enabled = true;
+            this.timerPoll.Interval = 50;
+            this.timerPoll.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // buttonReset
+            // 
+            this.buttonReset.Location = new System.Drawing.Point(15, 72);
+            this.buttonReset.Name = "buttonReset";
+            this.buttonReset.Size = new System.Drawing.Size(75, 23);
+            this.buttonReset.TabIndex = 4;
+            this.buttonReset.Text = "Reset";
+            this.buttonReset.UseVisualStyleBackColor = true;
+            this.buttonReset.Click += new System.EventHandler(this.buttonReset_Click);
+            // 
+            // buttonStatus
+            // 
+            this.buttonStatus.Location = new System.Drawing.Point(15, 110);
+            this.buttonStatus.Name = "buttonStatus";
+            this.buttonStatus.Size = new System.Drawing.Size(75, 23);
+            this.buttonStatus.TabIndex = 5;
+            this.buttonStatus.Text = "Status";
+            this.buttonStatus.UseVisualStyleBackColor = true;
+            this.buttonStatus.Click += new System.EventHandler(this.buttonStatus_Click);
+            // 
+            // textBoxCarrier
+            // 
+            this.textBoxCarrier.Location = new System.Drawing.Point(15, 150);
+            this.textBoxCarrier.Name = "textBoxCarrier";
+            this.textBoxCarrier.Size = new System.Drawing.Size(98, 22);
+            this.textBoxCarrier.TabIndex = 6;
+            this.textBoxCarrier.Text = "38000";
+            // 
+            // textBoxDutyCycle
+            // 
+            this.textBoxDutyCycle.Location = new System.Drawing.Point(13, 179);
+            this.textBoxDutyCycle.Name = "textBoxDutyCycle";
+            this.textBoxDutyCycle.Size = new System.Drawing.Size(100, 22);
+            this.textBoxDutyCycle.TabIndex = 7;
+            this.textBoxDutyCycle.Text = "33";
+            // 
+            // buttonCarrier
+            // 
+            this.buttonCarrier.Location = new System.Drawing.Point(15, 208);
+            this.buttonCarrier.Name = "buttonCarrier";
+            this.buttonCarrier.Size = new System.Drawing.Size(75, 23);
+            this.buttonCarrier.TabIndex = 8;
+            this.buttonCarrier.Text = "Carrier";
+            this.buttonCarrier.UseVisualStyleBackColor = true;
+            this.buttonCarrier.Click += new System.EventHandler(this.buttonCarrier_Click);
+            // 
+            // textBoxTargetNEC
+            // 
+            this.textBoxTargetNEC.Location = new System.Drawing.Point(15, 263);
+            this.textBoxTargetNEC.Name = "textBoxTargetNEC";
+            this.textBoxTargetNEC.Size = new System.Drawing.Size(98, 22);
+            this.textBoxTargetNEC.TabIndex = 9;
+            // 
+            // textBoxCmdNEC
+            // 
+            this.textBoxCmdNEC.Location = new System.Drawing.Point(15, 292);
+            this.textBoxCmdNEC.Name = "textBoxCmdNEC";
+            this.textBoxCmdNEC.Size = new System.Drawing.Size(98, 22);
+            this.textBoxCmdNEC.TabIndex = 10;
+            // 
+            // buttonSendNEC
+            // 
+            this.buttonSendNEC.Location = new System.Drawing.Point(15, 321);
+            this.buttonSendNEC.Name = "buttonSendNEC";
+            this.buttonSendNEC.Size = new System.Drawing.Size(86, 23);
+            this.buttonSendNEC.TabIndex = 11;
+            this.buttonSendNEC.Text = "Send NEC";
+            this.buttonSendNEC.UseVisualStyleBackColor = true;
+            this.buttonSendNEC.Click += new System.EventHandler(this.buttonSendNEC_Click);
+            // 
+            // AVRemoteExplorerForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(879, 563);
+            this.Controls.Add(this.buttonSendNEC);
+            this.Controls.Add(this.textBoxCmdNEC);
+            this.Controls.Add(this.textBoxTargetNEC);
+            this.Controls.Add(this.buttonCarrier);
+            this.Controls.Add(this.textBoxDutyCycle);
+            this.Controls.Add(this.textBoxCarrier);
+            this.Controls.Add(this.buttonStatus);
+            this.Controls.Add(this.buttonReset);
+            this.Controls.Add(this.labelToWrite);
+            this.Controls.Add(this.labelToRead);
+            this.Controls.Add(this.listBoxInfo);
+            this.Name = "AVRemoteExplorerForm";
+            this.Text = "AV Remote Explorer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.IO.Ports.SerialPort serialPortAVR;
+        private System.Windows.Forms.ListBox listBoxInfo;
+        private System.Windows.Forms.Label labelToRead;
+        private System.Windows.Forms.Label labelToWrite;
+        private System.Windows.Forms.Timer timerPoll;
+        private System.Windows.Forms.Button buttonReset;
+        private System.Windows.Forms.Button buttonStatus;
+        private System.Windows.Forms.TextBox textBoxCarrier;
+        private System.Windows.Forms.TextBox textBoxDutyCycle;
+        private System.Windows.Forms.Button buttonCarrier;
+        private System.Windows.Forms.TextBox textBoxTargetNEC;
+        private System.Windows.Forms.TextBox textBoxCmdNEC;
+        private System.Windows.Forms.Button buttonSendNEC;
+    }
+}
+
---
+++ GenerateSerial/AVRemoteExplorerForm.cs
@@ -0,0 +1,305 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace GenerateSerial
+{
+    public partial class AVRemoteExplorerForm : Form
+    {
+        public AVRemoteExplorerForm()
+        {
+            InitializeComponent();
+            serialPortAVR.Open();
+            serialPortAVR.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(serialPort1_DataReceived);
+            Application.Idle += new EventHandler(Application_Idle);
+        }
+
+        int oldbtr = -1;
+        int oldbtw = -1;
+
+        void Application_Idle(object sender, EventArgs e)
+        {
+            int btr = serialPortAVR.BytesToRead;
+            if (btr != oldbtr)
+            {
+                labelToRead.Text = btr.ToString();
+            }
+            int btw = serialPortAVR.BytesToWrite;
+            if (btw != oldbtw)
+            {
+                labelToWrite.Text = btw.ToString();
+            }
+            if (btr > 0)
+            {
+                DoRead();
+            }
+            lock (ibuf)
+            {
+                int n = ibuf.Count;
+                if (n > 0 && ibuf[0] <= n)
+                {
+                    //  decode a command
+                    if (ibuf[0] == 0)
+                    {
+                        //  null command
+                        ibuf.RemoveRange(0, 1);
+                        listBoxInfo.Items.Add("-> null cmd");
+                    }
+                    else
+                    {
+                        sbyte[] payload = null;
+                        if (ibuf[0] > 2)
+                        {
+                            payload = new sbyte[ibuf[0] - 2];
+                            ibuf.CopyTo(2, payload, 0, ibuf[0] - 2);
+                        }
+                        this.on_response(ibuf[1], payload);
+                        ibuf.RemoveRange(0, ibuf[0]);
+                    }
+                }
+            }
+        }
+
+        void serialPort1_DataReceived(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
+        {
+            DoRead();
+        }
+
+        void DoRead()
+        {
+            lock (ibuf)
+            {
+                int btr = serialPortAVR.BytesToRead;
+                byte[] buf = new byte[btr];
+                serialPortAVR.Read(buf, 0, btr);
+                //  this is terrible!
+                foreach (byte b in buf)
+                {
+                    ibuf.Add((sbyte)b);
+                }
+            }
+        }
+
+        List<sbyte> ibuf = new List<sbyte>();
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Application_Idle(sender, e);
+        }
+
+        public void on_response(sbyte cmd, sbyte[] data)
+        {
+            switch (cmd)
+            {
+                case 1:
+                    on_error(data);
+                    break;
+                case 2:
+                    on_status(data);
+                    break;
+                default:
+                    on_unknown(cmd, data);
+                    break;
+            }
+        }
+
+        public unsafe static string stringFromData(sbyte[] data)
+        {
+            if (data == null)
+            {
+                return "";
+            }
+            fixed (sbyte *sp = data)
+            {
+                return new String(sp, 0, data.Length);
+            }
+        }
+
+        public static string hexdump(sbyte[] data)
+        {
+            StringBuilder sf = new StringBuilder();
+            for (int i = 0, n = data.Length; i != n; ++i)
+            {
+                sf.AppendFormat("{0:X2} ", data[i]);
+            }
+            return sf.ToString();
+        }
+
+        public void on_error(sbyte[] data)
+        {
+            listBoxInfo.Items.Add(String.Format("-> error('{0}')", stringFromData(data)));
+        }
+
+        public void on_status(sbyte[] data)
+        {
+            listBoxInfo.Items.Add(String.Format("-> status('{0}')", stringFromData(data)));
+        }
+
+        public void on_unknown(sbyte code, sbyte[] data)
+        {
+            listBoxInfo.Items.Add(String.Format("-> unknown code {0} ({1})", code, hexdump(data)));
+        }
+
+        public void send_cmd(byte cmd, byte[] data)
+        {
+            byte len = 2;
+            if (data != null)
+            {
+                if (data.Length > 30)
+                {
+                    throw new InvalidOperationException("Maximum payload size is 30 bytes");
+                }
+                len = (byte)(len + data.Length);
+            }
+            serialPortAVR.Write(new byte[] { len, cmd }, 0, 2);
+            if (data != null)
+            {
+                serialPortAVR.Write(data, 0, data.Length);
+            }
+        }
+
+        public void send_reset()
+        {
+            send_cmd(0, null);
+        }
+
+        public void send_status()
+        {
+            send_cmd(1, null);
+        }
+
+        public void send_carrier(ushort carrier, byte dutycycle)
+        {
+            send_cmd(2, new byte[] { (byte)(carrier >> 8), (byte)(carrier & 0xff), dutycycle });
+        }
+
+        ushort[] codes = new ushort[200];
+        int nCodes = 0;
+        byte[] codeBytes = new byte[400];
+
+        private void addCode(bool high, ushort us)
+        {
+            if (nCodes == codes.Length)
+            {
+                throw new InvalidOperationException("addCode() -- too many codes!");
+            }
+            //  I only have 8 us precision
+            codes[nCodes] = (ushort)(us >> 3);
+            if (high)
+            {
+                codes[nCodes] |= 0x8000;
+            }
+        }
+
+        private void addBitsNEC(uint data, int count)
+        {
+            for (int i = 0; i < count; ++i)
+            {
+                addCode(true, 563);
+                if ((data & (1 << i)) != 0)
+                {
+                    addCode(false, 1125);
+                }
+                else
+                {
+                    addCode(false, 563);
+                }
+            }
+        }
+
+        private void send_codes()
+        {
+            for (int i = 0; i < nCodes; ++i)
+            {
+                codeBytes[i * 2] = (byte)(codes[i] >> 8);
+                codeBytes[i * 2 + 1] = (byte)(codes[i] & 0xffu);
+            }
+            send_cmd(3, new byte[] { (byte)nCodes });
+            //  that command is followed by un-framed code data
+            serialPortAVR.Write(codeBytes, 0, nCodes * 2);
+            nCodes = 0;
+        }
+
+        public void send_nec(ushort target, byte cmd)
+        {
+            nCodes = 0;
+
+            //  AGC preamble
+            addCode(true, 9000);
+            addCode(false, 2250);
+
+            //  16 bit address
+            addBitsNEC(target, 16);
+            //  8 bit command
+            addBitsNEC(cmd, 8);
+            //  8 bit command, inverted
+            addBitsNEC(cmd ^ 0xffu, 8);
+            send_codes();
+        }
+
+        public static ushort UInt16Parse(String s)
+        {
+            if (s.Length > 2 && s[0] == '0' && (s[1] == 'x' || s[1] == 'X'))
+            {
+                return UInt16.Parse(s.Substring(2), System.Globalization.NumberStyles.HexNumber);
+            }
+            return UInt16.Parse(s);
+        }
+
+        public static byte byteParse(String s)
+        {
+            if (s.Length > 2 && s[0] == '0' && (s[1] == 'x' || s[1] == 'X'))
+            {
+                return byte.Parse(s.Substring(2), System.Globalization.NumberStyles.HexNumber);
+            }
+            return byte.Parse(s);
+        }
+
+        private void buttonReset_Click(object sender, EventArgs e)
+        {
+            listBoxInfo.Items.Add("Reset");
+            this.send_reset();
+        }
+
+        private void buttonStatus_Click(object sender, EventArgs e)
+        {
+            listBoxInfo.Items.Add("Status");
+            this.send_status();
+        }
+
+        private void buttonCarrier_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                ushort carrier = UInt16Parse(textBoxCarrier.Text);
+                byte dutycycle = byteParse(textBoxDutyCycle.Text);
+                listBoxInfo.Items.Add(String.Format("carrier {0} dutycycle {1}", carrier, dutycycle));
+                this.send_carrier(carrier, dutycycle);
+            }
+            catch (System.Exception x)
+            {
+                MessageBox.Show(x.Message);
+            }
+        }
+
+        private void buttonSendNEC_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                ushort target = UInt16Parse(textBoxTargetNEC.Text);
+                byte cmd = byteParse(textBoxCmdNEC.Text);
+                listBoxInfo.Items.Add(String.Format("NEC target {0} cmd {1}", target, cmd));
+                this.send_nec(target, cmd);
+            }
+            catch (System.Exception x)
+            {
+                MessageBox.Show(x.Message);
+            }
+        }
+    }
+}
